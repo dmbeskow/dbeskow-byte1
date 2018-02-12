1762c4facdd69956aa044d19c4eb75a20dd088ed
David Beskow <dbeskow@andrew.cmu.edu>
2018-02-11 22:14:19 -0500

    final push

--- main.py
+++ main.py
@@ -17,24 +17,31 @@ import os
 import jinja2
 import webapp2
 import logging
+
 JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/templates"),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
 
-#class MainPage(webapp2.RequestHandler):
-#    def get(self):
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+    	template = JINJA_ENVIRONMENT.get_template('index.html')
+    	self.response.out.write(template.render())
 #        self.response.headers['Content-Type'] = 'text/plain'
 #        self.response.write('Hello, World2!')
 
 
-#app = webapp2.WSGIApplication([
-#    ('/', MainPage),
-#], debug=True)
+class aboutPage(webapp2.RequestHandler):
+    def get(self):
+    	template = JINJA_ENVIRONMENT.get_template('about.html')
+    	self.response.out.write(template.render())
 
 
-@app.route('/')
-def hello():
-    template = JINJA_ENVIRONMENT.get_template('templates/index.html')
-    return template.render()
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/about.html', aboutPage)
+], debug=True)
+
+
+
